de3f968ea2fd9b0ab8a0ff2c14d012899984435c
rafaelswr <rafaels.borges91@gmail.com>
2024-01-21 23:51:28 +0000

    create the role class to classify each employee + repository
    create the role class to classify each employee + repository

---
+++ backend/src/main/java/com/security/rafaelswr/models/Role.java
@@ -0,0 +1,42 @@
+package com.security.rafaelswr.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.net.PasswordAuthentication;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    private String authority;
+
+    @ManyToMany(mappedBy = "authorities", fetch = FetchType.EAGER)
+    @JsonBackReference
+    private Set<Employee> employees;
+
+    public Role(String authority){
+        this.authority = authority;
+        this.employees = new HashSet<>();
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
+
+}
--- backend/src/main/java/com/security/rafaelswr/repositories/EmployeeRepository.java
+++ backend/src/main/java/com/security/rafaelswr/repositories/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.security.rafaelswr.repositories;
 
 import com.security.rafaelswr.models.Employee;
+import com.security.rafaelswr.models.EmployeeInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,7 +9,9 @@ import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-    @Query("SELECT s from Employee s where s.email = ?1")
-    Optional<Employee> findByEmail(String employee);
+    @Query("SELECT s from EmployeeInfo s where s.email = ?1")
+    Optional<Employee> findByEmail(String email);
+
+
 
 }
